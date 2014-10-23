4389aaebe0bab3e9579a4da9361e527c0f0f5756
AlexanderNikolov <this.nikolov@gmail.com>
2014-10-23 13:43:35 +0300

    update

---
+++ BGCoder.com/Binary Digits Count.cs
@@ -0,0 +1,37 @@
+﻿namespace BGCoder.com
+{
+    using System;
+    using System.Linq;
+
+    class BinaryDigitsTask
+    {
+        static void Main(string[] args)
+        {
+            char digitB = char.Parse(Console.ReadLine());
+            int inputLinesCount = int.Parse(Console.ReadLine());
+
+            int[] resultsArray = new int[inputLinesCount];
+            int count = 0;
+
+            for (int i = 0; i < inputLinesCount; i++)
+            {
+                long input = long.Parse(Console.ReadLine());
+
+                string binaryString = Convert.ToString(input, 2);
+
+                for (int j = 0; j < binaryString.Length; j++)
+                {
+                    if (binaryString[j] == digitB)
+                    {
+                        count++;
+                    }
+                }
+
+                resultsArray[i] = count;
+                count = 0;
+            }
+
+            Console.Write(string.Join("\n", resultsArray));
+        }
+    }
+}

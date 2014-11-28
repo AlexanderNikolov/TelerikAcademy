aa82e97e699702bcce1f830aefdbaf54533e6fdc
AlexanderNikolov <this.nikolov@gmail.com>
2014-11-28 16:19:23 +0200

    new

---
+++ C#/C# Fundamentals/8. Multidimensional arrays/TAOnline_8_MultidimensionalArrays/FillMatrix.cs
@@ -0,0 +1,131 @@
+﻿/*
+ * Write a program that fills and prints a matrix of size (n, n) 
+ * as shown below: (examples for n = 4)
+ */
+
+namespace TAOnline_8_MultidimensionalArrays
+{
+    using System;
+    using System.Linq;
+
+    class FillMatrix
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please input matrix size N (n, n) ");
+            string input = Console.ReadLine();
+            int sizeN = Convert.ToInt32(input);
+
+            int[,] matrix = new int[sizeN, sizeN];
+
+            //variant A
+            Console.WriteLine();
+            int value = 1;
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    matrix[row, col] = value++;
+                }
+            }
+            
+            PrintArray(matrix);
+
+
+            //variant B
+            Console.WriteLine();
+            value = 1;
+            bool direction = true;
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                if (direction)
+                {
+                    for (int row = 0; row < matrix.GetLength(0); row++)
+                        matrix[row, col] = value++;
+                }
+                else
+                {
+                    for (int row = matrix.GetLength(0) - 1; row >= 0; row--)
+                        matrix[row, col] = value++;
+                }
+
+                direction = !direction;
+            }
+            
+            PrintArray(matrix);
+
+
+            //variant C
+            value = 1;
+            Console.WriteLine();
+
+            for (int row = sizeN - 1; row >= 0; row--)
+            {
+                for (int col = 0; col < sizeN - row; col++)
+                {
+                    matrix[row + col, col] = value++;
+                }
+            }
+            for (int col = 1; col < sizeN; col++)
+            {
+                for (int row = 0; row < sizeN - col; row++)
+                {
+                    matrix[row, col + row] = value++;
+                }
+            }
+            
+            PrintArray(matrix);
+
+
+            //varian D
+            Console.WriteLine();
+            value = 1;
+            int changeDirection = -sizeN;
+            int count = sizeN;
+            int index = -1;
+
+            do 
+            {
+                changeDirection = -1 * changeDirection / sizeN;
+
+                for (int i = 0; i < count; i++)
+                {
+                    index += changeDirection;
+                    matrix[index % sizeN, index / sizeN] = value++;
+                }
+
+                changeDirection *= sizeN;
+                count--;
+
+                for (int j = 0; j < count; j++)
+                {
+                    index += changeDirection;
+                    matrix[index % sizeN, index / sizeN] = value++;
+                }
+
+            } while (count > 0);
+
+            PrintArray(matrix);
+        }
+
+        /// <summary>
+        /// Print method with correct visualization of multidimensional arrays
+        /// </summary>
+        static void PrintArray<T>(T[,] array)
+        {
+            //calc whitespaces between columns
+            int space = (array.GetLength(0) * array.GetLength(1) - 1).ToString().Length + 1;
+
+            for (var i = 0; i < array.GetLength(0); i++)
+            {
+                for (var j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j].ToString().PadLeft(space, ' '));
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C#/C# Fundamentals/8. Multidimensional arrays/TAOnline_8_MultidimensionalArrays/Task3/LongestSeqEqualStringsMatrix.cs
@@ -0,0 +1,212 @@
+﻿/*
+ * We are given a matrix of strings of size N x M. Sequences in 
+ * the matrix we define as sets of several neighbor elements located
+ * on the same line, column or diagonal. Write a program that finds 
+ * the longest sequence of equal strings in the matrix 
+ */
+
+namespace TAOnline_8_MultidimensionalArrays
+{
+    using System;
+    using System.Linq;
+
+    class LongestSeqEqualStringsMatrix
+    {
+        static void Main(string[] args)
+        {
+            
+            string[,] matrix =  
+            {
+                {"ha", "fifi", "ho",   "hi"}, 
+                {"fo", "ha",   "hihi", "xx"},
+                {"vi", "ho",   "ha",   "xx"},
+            };
+
+            int bestIndexCol = 0;
+            int bestIndexRow = 0;
+            int bestLength = 0;
+
+            //searching in columns
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int length = 1;
+                int indexCol = 0;
+
+                for (int j = 1; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, indexCol] == matrix[i, j])
+                    {
+                        length++;
+                    }
+                    else
+                    {
+                        if (length > 2)
+                        {
+                            if (bestLength < length)
+                            {
+                                bestIndexCol = indexCol;
+                                bestIndexRow = i;
+                                bestLength = length;
+                            }
+                        }
+
+                        indexCol = j;
+                        length = 1;
+                    }
+
+                    //in case if subset scrab end of loop
+                    if (length > 2)
+                    {
+                        if (bestLength < length)
+                        {
+                            bestIndexCol = indexCol;
+                            bestIndexRow = i;
+                            bestLength = length;
+                        }
+                    }
+                }
+            }
+
+            //searching in rows
+            for (int ii = 0; ii < matrix.GetLength(1); ii++)
+            {
+                int length = 1;
+                int indexRow = 0;
+                for (int jj = 1; jj < matrix.GetLength(0); jj++)
+                {
+                    if (matrix[indexRow, ii] == matrix[jj, ii]) 
+                    {
+                        length++;
+                    }
+                    else
+                    {
+                        if (length > 2)
+                        {
+                            if (bestLength < length)
+                            {
+                                bestIndexCol = ii;
+                                bestIndexRow = indexRow;
+                                bestLength = length;
+                            }
+                        }
+
+                        indexRow = jj;
+                        length = 1;
+                    }
+                    //in case if subset scrab end of loop
+                    if (length > 2)
+                    {
+                        if (bestLength < length)
+                        {
+                            bestIndexCol = ii;
+                            bestIndexRow = indexRow;
+                            bestLength = length;
+                        }
+                    }
+                }
+            }
+
+            //searching diagonals
+            int limit = Math.Abs(matrix.GetLength(0) - matrix.GetLength(1)) + 1;
+
+            for (int iter = 0; iter < limit; iter++)
+            {
+                int length = 1;
+                for (int step = 1; step < matrix.GetLength(1) - 1; step++)
+                {
+                    if (matrix[step - 1, step - 1] == matrix[step, step])
+                    {
+                        length++;
+                    }
+                    else
+                    {
+                        if (length > 2)
+                        {
+                            if (bestLength < length)
+                            {
+                                bestIndexCol = iter;
+                                bestIndexRow = iter;
+                                bestLength = length;
+                            }
+                        }
+                    }
+                    //in case if subset scrab end of loop
+                    if (length > 2)
+                    {
+                        if (bestLength < length)
+                        {
+                            bestIndexCol = iter;
+                            bestIndexRow = iter;
+                            bestLength = length;
+                        }
+                    }
+                }
+            }
+
+            int count = 1;
+            string result = "";
+            for (int i = 0; i <= matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j <= matrix.GetLength(1); j++)
+                {
+                    int col = j;
+                    int row = i;
+
+                    if (col >= matrix.GetLength(1) - 1 || row >= matrix.GetLength(0) - 1)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        while (col > 0 && row < matrix.GetLength(0) - 1)
+                        {
+                            if (matrix[row, col] == matrix[row + 1, col - 1])
+                            {
+                                count++;
+                                if (count >= bestLength)
+                                {
+                                    result = matrix[row, col];
+                                    bestLength = count;
+                                }
+                                
+                                row++;
+                                col--;
+                            }
+                            else
+                            {
+                                count = 1;
+                                break;
+                            }
+                        }
+
+                        count = 1;
+                    }
+                }
+            }
+
+            //output
+            for (int index = 0; index < bestLength; index++)
+            {
+                Console.Write(matrix[bestIndexRow, bestIndexCol] + " ");
+            }
+            
+            Console.WriteLine();
+            PrintMatrix(matrix);
+        }
+
+        /// <summary>
+        /// Print method with correct visualisation
+        /// </summary>
+        /// <param name="matrix"></param>
+        static void PrintMatrix(string[,] matrix)
+        {
+            int cellSize = matrix[0, 0].Length;
+            foreach (string cell in matrix) cellSize = Math.Max(cellSize, cell.Length);
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    Console.Write(matrix[i, j].PadRight(cellSize, ' ') +
+                                 (j != matrix.GetLength(1) - 1 ? " " : "\n"));
+        }
+    }
+}

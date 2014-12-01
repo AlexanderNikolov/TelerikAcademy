5e5a4220a3ce83ef723a200b188804ee1909bc17
AlexanderNikolov <this.nikolov@gmail.com>
2014-12-01 17:16:46 +0200

    new task

---
+++ C#/C# Fundamentals/8. Multidimensonal Arrays/Task6/Matrix.cs
@@ -0,0 +1,111 @@
+﻿/*
+ * Write a class Matrix, to holds a matrix of integers. Overload 
+ * the operators for adding, subtracting and multiplying of matrices, 
+ * indexer for accessing the matrix content and ToString().
+ */
+
+using System;
+
+class Matrix
+{
+    private int[,] matrix;
+
+    /// <summary>
+    /// Class constructor
+    /// </summary>
+    public Matrix(int rows, int columns)
+    {
+        this.matrix = new int[rows, columns];
+    }
+
+    #region Properties of class.
+    public int Rows
+    {
+        get { return this.matrix.GetLength(0); }
+    }
+
+    public int Columns
+    {
+        get { return this.matrix.GetLength(1); }
+    }
+    #endregion
+
+    /// <summary>
+    /// Indexer
+    /// </summary>
+    public int this[int row, int col]
+    {
+        get 
+        { 
+            return this.matrix[row, col]; 
+        }
+        set 
+        { 
+            this.matrix[row, col] = value; 
+        }
+    }
+
+    #region Overloads. Up to many
+    public override string ToString()
+    {
+        string result = null;
+
+        for (int row = 0; row < this.Rows; row++)
+        {
+            for (int col = 0; col < this.Columns; col++)
+                result += matrix[row, col] + " ";
+            result += Environment.NewLine;
+        }
+
+        return result;
+    }
+
+    public static Matrix operator +(Matrix first, Matrix second)
+    {
+        Matrix result = new Matrix(first.Rows, first.Columns);
+
+        for (int row = 0; row < first.Rows; row++)
+        {
+            for (int col = 0; col < first.Columns; col++)
+            {
+                result[row, col] = first[row, col] + second[row, col];
+            }
+        }
+
+        return result;
+    }
+
+    public static Matrix operator -(Matrix first, Matrix second)
+    {
+        Matrix result = new Matrix(first.Rows, first.Columns);
+
+        for (int row = 0; row < first.Rows; row++)
+        {
+            for (int col = 0; col < first.Columns; col++)
+            {
+                result[row, col] = first[row, col] - second[row, col];
+            }
+        }
+
+        return result;
+    }
+
+    public static Matrix operator *(Matrix first, Matrix second)
+    {
+        Matrix result = new Matrix(first.Rows, second.Columns);
+
+        for (int row = 0; row < result.Rows; row++)
+        {
+            for (int col = 0; col < result.Columns; col++)
+            {
+                for (int k = 0; k < first.Columns; k++)
+                {
+                    result[row, col] = first[row, k] * second[k, col];
+                }
+            }
+        }
+        return result;
+    }
+    #endregion
+}
+
---
+++ C#/C# Fundamentals/8. Multidimensonal Arrays/Task6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Matrix matrixOne = new Matrix(2, 2);
+        matrixOne[0, 0] = 2;
+        matrixOne[0, 1] = -12;
+
+        Matrix matrixTwo = new Matrix(2, 2);
+        matrixTwo[0, 0] = 2;
+        matrixTwo[1, 1] = 5;
+
+        //test class
+        Matrix add = matrixOne + matrixTwo;
+        Matrix sub = matrixOne - matrixTwo;
+        Matrix multiplication = matrixOne * matrixTwo;
+
+        //output
+        Console.WriteLine(add.ToString());
+        Console.WriteLine();
+        Console.WriteLine(sub.ToString());
+        Console.WriteLine();
+        Console.WriteLine(multiplication.ToString());
+    }
+}

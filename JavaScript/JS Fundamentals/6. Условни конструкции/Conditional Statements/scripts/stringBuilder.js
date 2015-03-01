ef4814688dcfcc4b8f5612015d38f602183122aa
AlexanderNikolov <this.nikolov@gmail.com>
2015-03-01 13:32:06 +0200

    new entry

---
+++ JavaScript/JS Fundamentals/6. Условни конструкции/Conditional Statements/scripts/stringBuilder.js
@@ -0,0 +1,13 @@
+﻿function stringBuilder() {
+    return {
+        arr: [],
+        len: 0,
+        append: function (str) {
+            this.arr[this.len++] = str;
+            return this;
+        },
+        toString: function () {
+            return this.arr.join('');
+        }
+    };
+}
